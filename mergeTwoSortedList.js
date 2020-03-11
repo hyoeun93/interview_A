3a2a9cd8abe90f123314149bc97fb9abd9427eac
HyoEun Lee <hyoeun93@gmail.com>
2020-03-10 21:05:31 -0400

    mergeTwoSortedList

---
+++ mergeTwoSortedList.js
@@ -0,0 +1,28 @@
+
+var mergeTwoLists = function(l1, l2) {
+    if(!l1 || !l2) return l1 || l2;
+    const r = new ListNode();
+    let l = r;
+    while(l1 && l2) {
+        if(l1.val < l2.val) {
+            l.val = l1.val;
+            l1 = l1.next;
+        } else if (l1.val > l2.val) {
+            l.val = l2.val;
+            l2 = l2.next;
+        } else {
+            l.val = l1.val;
+            l.next = new ListNode(l2.val);
+            l = l.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        if(l1 && l2) {
+            l.next = new ListNode();
+            l = l.next;
+        }
+    }
+    if(l1) l.next = l1;
+    if(l2) l.next = l2;
+    return r;
+}
